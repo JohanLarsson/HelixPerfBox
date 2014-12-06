3bda7e245178e6ec0967ee495a9e4826d05e5f63
Johan Larsson <johan_gavle1@hotmail.com>
2014-12-06 11:10:08 +0100

    Initial

---
+++ HelixPerfBox/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+
+namespace HelixPerfBox
+{
+    using System.Diagnostics;
+    using System.Windows.Media;
+    using System.Windows.Media.Media3D;
+    using HelixToolkit.Wpf;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MeshBuilder _meshBuilder = new MeshBuilder();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void OnAdd100Click(object sender, RoutedEventArgs e)
+        {
+            var material = new DiffuseMaterial(Brushes.Green);
+
+            var stopwatch = Stopwatch.StartNew();
+            for (int i = 0; i < 100; i++)
+            {
+                var sphere = new SphereVisual3D
+                {
+                    Center = new Point3D(0, 0, i),
+                    Radius = 0.3,
+                    Material = material
+                };
+                Viewport3D.Children.Add(sphere);
+            }
+            Button.Content = stopwatch.ElapsedMilliseconds;
+            Viewport3D.ZoomExtents(100);
+        }
+    }
+}
