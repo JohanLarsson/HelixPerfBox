005d79d8a2700209d899be493c998a6a57d92c5d
Johan Larsson <johan_gavle1@hotmail.com>
2014-12-06 17:29:34 +0100

    Stuck

--- HelixPerfBox/Selector3D.cs
+++ HelixPerfBox/Selector3D.cs
@@ -11,6 +11,7 @@
     using System.Windows.Controls.Primitives;
     using System.Windows.Data;
     using System.Windows.Input;
+    using System.Windows.Media;
     using System.Windows.Media.Media3D;
     using Gu.Reactive;
     using HelixToolkit.Wpf;
@@ -23,14 +24,6 @@
                 null,
                 FrameworkPropertyMetadataOptions.AffectsRender,
                 OnItemsSourceChanged));
-
-        private readonly ContainerUIElement3D _container;
-
-        public Selector3D()
-        {
-            _container = new ContainerUIElement3D();
-            Children.Add(_container);
-        }
 
         public IEnumerable<object> ItemsSource
         {
@@ -51,75 +44,48 @@
             var meshes = await Task.Run(() => balls.Select(b => Tuple.Create(b, CreateMesh(b))).ToArray());
             Debug.WriteLine("Create meshes: {0} ms", stopwatch.ElapsedMilliseconds);
             stopwatch.Restart();
-            foreach (var mesh in meshes)
+            foreach (var tuple in meshes)
             {
+                var ball = tuple.Item1;
+                var geometry = tuple.Item2;
+                var material = new DiffuseMaterial(ball.Brush);
+                Bind(material, DiffuseMaterial.BrushProperty, ball, x => x.Brush);
 
-                var element = new ModelUIElement3D();
-                var ball = mesh.Item1;
-                var geometry = new GeometryModel3D
-                {
-                    Geometry = mesh.Item2,
-                    Material = new DiffuseMaterial(ball.Brush)
-                };
-                element.Model = geometry;
-                Bind(geometry.Material, DiffuseMaterial.BrushProperty, ball, x => x.Brush);
-                //Bind(sphere, SphereVisual3D.CenterProperty, ball, x => x.Point3D);
-                //Bind(sphere, SphereVisual3D.RadiusProperty, ball, x => x.Radius);
-                //element.Transform = new TranslateTransform3D(5, 0, 0);
-                element.MouseDown += selector3D.ContainerElementMouseDown;
-                selector3D._container.Children.Add(element);
+                var model = new GeometryModel3D { Geometry = geometry, Material = material };
+                var element = new ModelUIElement3D { Model = model };
+                element.AddHandler(UIElement3D.MouseDownEvent, new RoutedEventHandler(selector3D.ContainerElementMouseDown), true);
+                element.MouseLeftButtonDown += selector3D.ContainerElementMouseDown;
+                //var parent =(Viewport3DVisual) VisualTreeHelper.GetParent(selector3D);
+               
+                selector3D.Children.Add(element);
             }
             Debug.WriteLine("Add points: {0} ms", stopwatch.ElapsedMilliseconds);
-
-            //selector3D.AddRange(balls);
-            //var list = Task.Run(() =>
-            //{
-            //    var visual3Ds = new List<Tuple<SphereVisual3D,Ball>>();
-            //    foreach (var ball in balls)
-            //    {
-            //        var sphere = new SphereVisual3D
-            //        {
-            //            Material = new DiffuseMaterial(ball.Brush),
-            //            Center = ball.Point3D,
-            //            Radius = ball.Radius
-            //        };
-            //        //Bind(material, DiffuseMaterial.BrushProperty, ball, x => x.Brush);
-            //        //Bind(sphere, SphereVisual3D.CenterProperty, ball, x => x.Point3D);
-            //        //Bind(sphere, SphereVisual3D.RadiusProperty, ball, x => x.Radius);
-            //        var tuple = Tuple.Create( sphere,ball);
-            //        visual3Ds.Add(tuple);
-            //    }
-            //    return visual3Ds;
-            //});
-            //selector3D.Dispatcher.Invoke(() =>
-            //{
-            //    foreach (var visual3D in list)
-            //    {
-            //        selector3D.Children.Add(visual3D.Item1);
-            //    }
-            //});
-
         }
-        private void ContainerElementMouseDown(object sender, MouseButtonEventArgs e)
+
+        private void ContainerElementMouseDown(object sender, RoutedEventArgs e)
         {
+            var element = (ModelUIElement3D)sender;
+            var gm = (GeometryModel3D)element.Model;
+            gm.Material = Equals(gm.Material, Materials.Blue) ? Materials.Red : Materials.Blue;
+            e.Handled = true;
         }
 
-        private void AddRange(IEnumerable<Ball> balls)
-        {
-            foreach (var ball in balls)
-            {
-                var sphere = new SphereVisual3D
-                {
-                    Material = new DiffuseMaterial(ball.Brush),
-                    Center = ball.Point3D,
-                    Radius = ball.Radius
-                };
-                Bind(sphere.Material, DiffuseMaterial.BrushProperty, ball, x => x.Brush);
-                Bind(sphere, SphereVisual3D.CenterProperty, ball, x => x.Point3D);
-                Bind(sphere, SphereVisual3D.RadiusProperty, ball, x => x.Radius);
-                Children.Add(sphere);
-            }
-        }
+        //private void AddRange(IEnumerable<Ball> balls)
+        //{
+        //    foreach (var ball in balls)
+        //    {
+        //        var sphere = new SphereVisual3D
+        //        {
+        //            Material = new DiffuseMaterial(ball.Brush),
+        //            Center = ball.Point3D,
+        //            Radius = ball.Radius
+        //        };
+        //        Bind(sphere.Material, DiffuseMaterial.BrushProperty, ball, x => x.Brush);
+        //        Bind(sphere, SphereVisual3D.CenterProperty, ball, x => x.Point3D);
+        //        Bind(sphere, SphereVisual3D.RadiusProperty, ball, x => x.Radius);
+        //        Children.Add(sphere);
+        //    }
+        //}
 
         private static void Bind<TSource>(DependencyObject target,
                                           DependencyProperty property,
@@ -137,9 +103,11 @@
 
         private static MeshGeometry3D CreateMesh(Ball ball)
         {
-            var meshBuilder = new MeshBuilder();
+            var meshBuilder = new MeshBuilder(false, true);
             meshBuilder.AddSphere(ball.Point3D, ball.Radius, 100, 50);
-            return meshBuilder.ToMesh(true);
+            var geometry3D = meshBuilder.ToMesh(true);
+            geometry3D.Freeze();
+            return geometry3D;
         }
     }
 }
