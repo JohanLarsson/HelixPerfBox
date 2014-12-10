24f8af768f16c6e6111ee82eed73417a997b8934
Johan Larsson <johan_gavle1@hotmail.com>
2014-12-10 13:10:59 +0100

    Playing with ~DataContext~ for freezable.

---
+++ HelixPerfBox/Reflection/FreezableExt.cs
@@ -0,0 +1,55 @@
+﻿namespace HelixPerfBox
+{
+    using System.Reflection;
+    using System.Windows;
+
+    public static class FreezableExt
+    {
+        private static readonly MethodInfo AddInheritanceContextMethod = typeof(Freezable).GetMethod("AddInheritanceContext", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy);       
+        private static readonly MethodInfo RemoveInheritanceContextMethod = typeof(Freezable).GetMethod("RemoveInheritanceContext", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
+
+        public static readonly DependencyProperty DataContextProxyProperty = DependencyProperty.RegisterAttached(
+            "DataContextProxy",
+            typeof(object),
+            typeof(FreezableExt),
+            new PropertyMetadata(default(object), OnDataContextChanged));
+
+        public static void SetDataContextProxy(this Freezable element, object value)
+        {
+            element.SetValue(DataContextProxyProperty, value);
+        }
+
+        public static object GetDataContextProxy(this Freezable element)
+        {
+            return (object)element.GetValue(DataContextProxyProperty);
+        }
+
+        /// <summary>
+        /// Helper function to add context information to a Freezable.
+        /// http://referencesource.microsoft.com/#WindowsBase/Base/System/Windows/Freezable.cs,1176
+        /// </summary>
+        /// <param name="context">The DependencyObject to add that references this Freezable.</param>
+        /// <param name="property">The property of the DependencyObject this object maps to or null if none.</param>
+        public static void AddInheritanceContextWithReflection(this Freezable self, DependencyObject context, DependencyProperty property)
+        {
+            AddInheritanceContextMethod.Invoke(self, new object[] { context, property });
+        }
+
+        /// <summary>
+        /// Helper function to remove context information to a Freezable.
+        /// http://referencesource.microsoft.com/#WindowsBase/Base/System/Windows/Freezable.cs,1204
+        /// </summary>
+        /// <param name="context">The DependencyObject to remove that references this Freezable.</param>
+        /// <param name="property">The property of the DependencyObject this object maps to or null if none.</param>
+        public static void RemoveInheritanceContextWithReflection(this Freezable self, DependencyObject context, DependencyProperty property)
+        {
+            RemoveInheritanceContextMethod.Invoke(self, new object[] { context, property });
+        }
+
+        private static void OnDataContextChanged(DependencyObject o, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var freezable = (Freezable)o;
+            freezable.AddInheritanceContextWithReflection(o, DataContextProxyProperty);
+        }
+    }
+}
---
+++ HelixPerfBoxTests/Sandbox.cs
@@ -0,0 +1,84 @@
+﻿namespace HelixPerfBoxTests
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    using System.Windows;
+
+    using HelixPerfBox;
+    using HelixPerfBox.Annotations;
+
+    using NUnit.Framework;
+
+    public class Sandbox
+    {
+        [Test, Explicit("This hangs dunno why")]
+        public void DataContext()
+        {
+            var freezableDummy = new FreezableDummy();
+            //var binding = new Binding("Name");
+            //var bindingExpressionBase = BindingOperations.SetBinding(freezableDummy, FreezableDummy.ValueProperty, binding);
+            //Assert.AreEqual(null, freezableDummy.Value);
+
+            var vm = new DummyVm { Name = "Test" };
+            freezableDummy.SetDataContextProxy(vm);
+            //Assert.AreEqual(vm, freezableDummy.GetValue(FrameworkElement.DataContextProperty));
+            //bindingExpressionBase.UpdateTarget();
+            //Assert.AreEqual("Test", freezableDummy.Value);
+        }
+    }
+
+    public class FreezableDummy : Freezable
+    {
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(string), typeof(FreezableDummy), new PropertyMetadata(default(string)));
+
+        public string Value
+        {
+            get
+            {
+                return (string)GetValue(ValueProperty);
+            }
+            set
+            {
+                SetValue(ValueProperty, value);
+            }
+        }
+        protected override Freezable CreateInstanceCore()
+        {
+            return new FreezableDummy();
+        }
+    }
+
+    public class DummyVm : INotifyPropertyChanged
+    {
+        private string _name;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value == _name)
+                {
+                    return;
+                }
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
